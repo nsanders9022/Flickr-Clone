65317028e2dcbf10daadac3d133fbd72e1633684
Katy Daviscourt and Nicole Sanders <katy+nicole@>
2017-04-26 11:20:48 -0700

    run a test for GetCaptionTest

---
+++ Flickr.Tests/PictureTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Flickr.Models;
+using Xunit;
+
+namespace Flickr.Tests
+{
+    public class PictureTest
+    {
+        [Fact]
+        public void GetCaptionTest()
+        {
+            //Arrange
+            var picture = new Picture();
+            picture.Caption = "Space!";
+
+            //Act
+            var result = picture.Caption;
+
+            //Assert
+            Assert.Equal("Space!", result);
+        }
+    }
+}
