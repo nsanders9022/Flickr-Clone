68f85e8bef7057054418b50a47dde9c8fd3088ef
Katy Daviscourt and Nicole Sanders <katy+nicole@>
2017-04-26 11:42:11 -0700

    pass first two tests

---
+++ Flickr.Tests/ControllerTests/ItemsControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Flickr.Controllers;
+using Flickr.Models;
+using Xunit;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace Flickr.Tests.ControllerTests
+{
+    public class PicturesControllerTest
+    {
+        private readonly FlickrDbContext _db;
+        private readonly UserManager<FlickrUser> _userManager;
+
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            PicturesController controller = new PicturesController(_userManager, _db);
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
