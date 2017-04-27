415c94ef23662a97a8232b91669715eb0faa9802
Katy Daviscourt and Nicole Sanders <katy+nicole@>
2017-04-27 10:40:26 -0700

    add CRUD and views for Tags

---
+++ src/Flickr/Controllers/TagsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Flickr.Models;
+
+
+namespace Flickr.Controllers
+{
+    public class TagsController : Controller
+    {
+        private FlickrDbContext _db = new FlickrDbContext();
+        // GET: /<controller>/
+        public IActionResult Create()
+        {
+            ViewBag.PictureId = new SelectList(_db.Pictures, "PictureId", "Caption");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Tag tag)
+        {
+            _db.Tags.Add(tag);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Pictures");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            ViewBag.TagId = new SelectList(_db.Tags, "PictureId", "Caption");
+            var thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+            return View(thisTag);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Tag tag)
+        {
+            _db.Entry(tag).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Pictures");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+            return View(thisTag);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+            _db.Tags.Remove(thisTag);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Pictures");
+        }
+    }
+}
