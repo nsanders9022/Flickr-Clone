d6a844041975c7559a5c1fc1e3aa1f7cbd2e3250
Katy Daviscourt and Nicole Sanders <katy+nicole@>
2017-04-27 10:47:39 -0700

    add a page to link a tag to a picture

---
+++ src/Flickr/Controllers/PicturesTagsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Flickr.Models;
+
+
+
+namespace Flickr.Controllers
+{
+    public class PicturesTagsController : Controller
+    {
+        private FlickrDbContext db = new FlickrDbContext();
+
+        //public IActionResult Index()
+        //{
+        //    return View(db.ExperiencesPeoples.ToList());
+        //}
+
+        public IActionResult Create()
+        {
+            ViewBag.PictureId = new SelectList(db.Pictures, "PictureId", "Caption");
+            ViewBag.TagId = new SelectList(db.Tags, "TagId", "Word");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(PictureTag pictureTag)
+        {
+            db.PicturesTags.Add(pictureTag);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Pictures");
+        }
+    }
+}
